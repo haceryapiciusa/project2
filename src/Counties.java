7a11d246112cd9d600c935d94516154b6942ff7d
haceryapiciusa <56479407+haceryapiciusa@users.noreply.github.com>
2019-11-25 23:58:52 -0500

    done

--- src/Counties.java
+++ src/Counties.java
@@ -11,7 +11,9 @@ public class Counties {
      * method should take a tree number(int) and return smallest number(int)
      * Write a Java method to find the smallest number among three numbers.
      */
-
+    public static int smallestNumber (int a, int b, int c){
+        return Math.min(Math.min(a,b),c);
+    }
 
     /** Create a method
      * method name is middleCharacter    <-------   METHOD NAME SHOULD BE AS THIS
@@ -19,6 +21,16 @@ public class Counties {
      * Write a Java method to display the middle character of a string
      * if the string length is even number return 0
      */
+
+    public  char middleCharacter (String a){
+        char [] array = a.toCharArray();
+        char x = array[array.length/2];
+        if (array.length %2 == 0 ){
+            return 0;
+        }else {
+            return x;
+        }
+    }
 
 
     /** Create a method
@@ -28,17 +40,40 @@ public class Counties {
      * if the number is 2 return tuesday
      * if the number bigger then 7 then return -->> this is not a expected input
      */
-
+    public String dayOfTheWeek (int i) {
+        if( i == 1){
+            return "Monday";
+        }else if(i == 2){
+            return "Tuesday";
+        }else if(i == 3){
+            return "Wednesday";
+        }else if(i == 4){
+            return "Thursday";
+        }else if(i == 5){
+            return "Friday";
+        }else if(i == 6){
+            return "Saturday";
+        } else if(i == 7 ){
+            return "Sunday";
+        } else
+        return "this is not a expected input";
+    }
 
     /**
      * if CountThis String in the myCounties ArrayList then return how many of CountThis string in the myCounties
      * if CountThis is not in the myCountries then return -1
      */
     public int countCountry(ArrayList<String> myCountries, String CountThis) {
-
+        if(!myCountries.contains(CountThis)){
+            return -1;
+        }
         int result = 0;
         // START THE CODE FROM HERE
-
+        for (String a : myCountries) {
+            if (a.equals(CountThis)){
+                result++;
+            }
+        }
 
         // CODE END HERE
         return result;
@@ -50,7 +85,7 @@ public class Counties {
      * result should be ====>>>   brazil england USA
      */
     public ArrayList<String> sortArrayList(ArrayList<String> myCountries) {
-
+        Collections.sort((myCountries));
 
         return myCountries;
     }
@@ -63,7 +98,10 @@ public class Counties {
     public ArrayList<String> reverseTheList(ArrayList<String> myCountries) {
         ArrayList<String> reverse = new ArrayList<>();
 //       start writing the code here
-
+        Collections.reverse(myCountries);
+        for (String x : myCountries) {
+            reverse.add(x);
+        }
 
 //        ends here
         return reverse;
@@ -81,9 +119,18 @@ public class Counties {
      * return false
      */
     public boolean isFirstArrayListContainsSecondArrayList(ArrayList<String> firstArray, ArrayList<String> SecondAray) {
+        boolean a = false;
+        for (String x : firstArray) {
+            for (String y : SecondAray) {
+                if(x == y){
+                    a = true;
+                }else
+                    a = false;
+            }
+        }
 
 
-        return false;
+        return a;
     }
 
     /**
@@ -95,8 +142,14 @@ public class Counties {
      */
     public ArrayList<String> replaceTheCountry(ArrayList<String> myCountries, String firstCountry, String SecondCounry) {
 
-
+        for (String x : myCountries) {
+            if(x.contains(firstCountry)){
+                int a = myCountries.indexOf(firstCountry);
+                myCountries.set(a,SecondCounry);
+            }
+        }
         return myCountries;
     }
+
 
 }
--- src/CountriesTest.java
+++ src/CountriesTest.java
@@ -200,52 +200,11 @@ public class CountriesTest {
 
     }
 
-    @Test
-    public void getTotal1() {
-
-        String myNum = "5421312341";
 
 
-        int myTotal = c1.getTotal(myNum);
-
-        int expected = 10;
 
 
-        Assert.assertTrue(myTotal == expected);
 
-
-    }
-
-    @Test
-    public void getTotal2() {
-
-        String myNum = "5654656213";
-
-
-        int myTotal = c1.getTotal(myNum);
-
-        int expected = 29;
-
-
-        Assert.assertTrue(myTotal == expected);
-
-
-    }
-
-    @Test
-    public void getTotal3() {
-
-        String myNum = "00000000000";
-
-
-        int myTotal = c1.getTotal(myNum);
-
-        int expected = -1;
-
-
-        Assert.assertTrue(myTotal == expected);
-
-    }
 
     @Test
     public void isfirstArrayListContainssecondArrayList1() {
@@ -383,19 +342,19 @@ public class CountriesTest {
 
     @Test
     public void replaceTheCountry3() {
-        ArrayList<String> myCount = new ArrayList<>();
+        ArrayList<String> myCountries = new ArrayList<>();
 
-        myCount.add("Finland");
-        myCount.add("USA");
-        myCount.add("Germany");
-        myCount.add("Finland");
-        myCount.add("USA");
+        myCountries.add("Finland");
+        myCountries.add("USA");
+        myCountries.add("Germany");
+        myCountries.add("Finland");
+        myCountries.add("USA");
 
-        String first = "South Africa";
-        String second = "Italy";
+        String firstCountry = "South Africa";
+        String SecondCounry = "Italy";
 
 
-        ArrayList<String> Actual = c1.replaceTheCountry(myCount, first, second);
+        ArrayList<String> Actual = c1.replaceTheCountry(myCountries, firstCountry, SecondCounry);
 
         ArrayList<String> Expected = new ArrayList<>();
 
@@ -408,7 +367,6 @@ public class CountriesTest {
         for (int i = 0; i < Expected.size(); i++) {
 
             Assert.assertTrue(Actual.get(i).equals(Expected.get(i)));
-
 
         }
 
@@ -447,7 +405,7 @@ public class CountriesTest {
     @Test
     public void middleCharater1() {
 
-        char i1 = c1.middleCharater("HeyDude");
+        char i1 = c1.middleCharacter("HeyDude");
 
         Assert.assertTrue(i1 == 'D');
 
@@ -456,7 +414,7 @@ public class CountriesTest {
     @Test
     public void middleCharater2() {
 
-        char i1 = c1.middleCharater("Cerry");
+        char i1 = c1.middleCharacter("Cerry");
 
         Assert.assertTrue(i1 == 'r');
 
@@ -466,16 +424,16 @@ public class CountriesTest {
     @Test
     public void middleCharater3() {
 
-        char i1 = c1.middleCharater("Ring");
+        char i1 = c1.middleCharacter("Ring");
 
-        Assert.assertTrue(i1 == '0');
+        Assert.assertTrue(i1 == 10);
 
     }
 
     @Test
     public void dayoftheWeek1() {
 
-        String i1 = c1.dayoftheWeek(1);
+        String i1 = c1.dayOfTheWeek(1);
 
         Assert.assertTrue(i1.equals("Monday"));
 
@@ -485,7 +443,7 @@ public class CountriesTest {
     @Test
     public void dayoftheWeek2() {
 
-        String i1 = c1.dayoftheWeek(5);
+        String i1 = c1.dayOfTheWeek(5);
 
         Assert.assertTrue(i1.equals("Friday"));
 
@@ -495,16 +453,16 @@ public class CountriesTest {
     @Test
     public void dayoftheWeek3() {
 
-        String i1 = c1.dayoftheWeek(0);
+        String i1 = c1.dayOfTheWeek(0);
 
         Assert.assertTrue(i1.equals("this is not a expected input"));
 
     }
 
     @Test
-    public void dayoftheWeek4() {
+    public void dayOfTheWeek() {
 
-        String i1 = c1.dayoftheWeek(12);
+        String i1 = c1.dayOfTheWeek(12);
 
         Assert.assertTrue(i1.equals("this is not a expected input"));
 
